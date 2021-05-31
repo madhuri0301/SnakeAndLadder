15b11841420d9321d094ffdf80873e7e6f1fdfc6
Madhuri <harshupatil0301@gmail>
2021-05-31 12:42:37 -0700

    Winning Position

--- SnakeAndLadderGame/Game.cs
+++ SnakeAndLadderGame/Game.cs
@@ -13,32 +13,41 @@ namespace SnakeAndLadderGame
         {
             Console.WriteLine("Let's Play");
             int start = 0;
+            int win = 100;
 
-            Random random = new Random();
-            int dice = random.Next(1, 7);  //dicerolled
-            Console.WriteLine("The Number Is = " + dice);
-
-            int check = random.Next(0, 3); //To Check it's Snake Or Ladder
-            switch (check)
+            while (start < win)
             {
-                case NO_PLAY:
-                    Console.WriteLine("No Play"); 
-                    break;
 
-                //ladder
-                case LADDER:
-                    Console.WriteLine("Heyy! You Found Ladder");
-                    start = start + dice;
-                    break;
+                Random random = new Random();
+                int dice = random.Next(1, 7);  //dicerolled
+                Console.WriteLine("The Number Is = " + dice);
 
-                //snake
-                case SNAKE:
-                    Console.WriteLine("ohh No! Snake!");
-                    start = start - dice;
-                    break;
+                int check = random.Next(0, 3); //To Check it's Snake Or Ladder
+                switch (check)
+                {
+                    case NO_PLAY:
+                        Console.WriteLine("No Play");
+                        break;
 
-                default:
-                    break;
+                    //ladder
+                    case LADDER:
+                        Console.WriteLine("Heyy! You Found Ladder");
+                        start = start + dice;
+                        break;
+
+                    //snake
+                    case SNAKE:
+                        Console.WriteLine("ohh No! Snake!");
+                        start = start - dice;
+                        if (start < 0)
+                            start = 0;
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.WriteLine("You Are On Position " + start);
+
             }
         }
     }
