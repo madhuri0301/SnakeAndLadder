2b2e084ca4a3e7ea59568cb9e474696fecdb0931
Madhuri <harshupatil0301@gmail>
2021-05-31 12:18:00 -0700

    Dice Rolled

--- SnakeAndLadderGame/Game.cs
+++ SnakeAndLadderGame/Game.cs
@@ -10,6 +10,10 @@ namespace SnakeAndLadderGame
         {
             Console.WriteLine("Let's Play");
             int start = 0;
+
+            Random random = new Random();
+            int dice = random.Next(1, 7);  //dicerolled
+            Console.WriteLine("The Number Is = " + dice);
         }
     }
 }
