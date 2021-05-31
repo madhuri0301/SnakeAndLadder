3ff14bad5d5327af251bfad998faac91716fbfdb
Madhuri <harshupatil0301@gmail>
2021-05-31 12:30:42 -0700

    Checked It's Snake Or Ladder

--- SnakeAndLadderGame/Game.cs
+++ SnakeAndLadderGame/Game.cs
@@ -6,6 +6,9 @@ namespace SnakeAndLadderGame
 {
     public class Game
     {
+        const int NO_PLAY = 0;
+        const int SNAKE = 1;
+        const int LADDER = 2;
         public void Play()
         {
             Console.WriteLine("Let's Play");
@@ -14,6 +17,29 @@ namespace SnakeAndLadderGame
             Random random = new Random();
             int dice = random.Next(1, 7);  //dicerolled
             Console.WriteLine("The Number Is = " + dice);
+
+            int check = random.Next(0, 3); //To Check it's Snake Or Ladder
+            switch (check)
+            {
+                case NO_PLAY:
+                    Console.WriteLine("No Play"); 
+                    break;
+
+                //ladder
+                case LADDER:
+                    Console.WriteLine("Heyy! You Found Ladder");
+                    start = start + dice;
+                    break;
+
+                //snake
+                case SNAKE:
+                    Console.WriteLine("ohh No! Snake!");
+                    start = start - dice;
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
